300a71192a1ab177db59bdcccd9d4d078938be92
mustafa <mustafa@mustafa.168.1.93>
2026-04-06 16:58:35 +0300

    UPDATE: types.h dosyasında analiz yapıldı. Dosyada neler olmalı neler olmamalı bunlara karar verilip yorum satırına yazıldı

--- core/include/abyss/types.h
+++ core/include/abyss/types.h
@@ -1,1 +1,6 @@
-// Ortak veri tipleri ve enum'lar. Tüm sistemlerde ortak tipler kullanılacak.
+// Ortak veri tipleri ve enum'lar. Tüm sistemlerde ortak tipler kullanılacak. Ağır dependency olmamasına dikkat edilecek. Enum Class kullanılacak. Type Safety.
+// hash tipi string olarak kabul edilebilir ama zayıf bir sistemdir. Binary stringe nazaran daha güçlü. timestamp string olarak kullanılabilir ama parse edilmesi ve
+// karşılaştırma yapılması daha maliyetlidir. DiffType'de MODIFIED'da olacak bunu unutma. Exit Codeler biraz daha açıklayıcı olmalı ERROR yerine ERR_NO_REPO tarzı
+// açıklayıcı şekilde olmalı. Dosya içeriği çok uzun olmamalı Types.h dosyası fazla şişirilmemeli minimal olmalı.
+#include <string>
+#include <cstdint>
